3b89dc6fc1da1cc409888501663246402bd4ebb9
dams161 <dams16198@gmail.com>
2014-03-30 21:32:42 +0200

    initial

--- cm.mk
+++ cm.mk
@@ -7,6 +7,9 @@ PRODUCT_RELEASE_NAME := idols
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit device configuration
 $(call inherit-product, device/Alcatel/idols/device_idols.mk)
 
--- device_idols.mk
+++ device_idols.mk
@@ -9,13 +9,37 @@ DEVICE_PACKAGE_OVERLAYS += device/Alcatel/idols/overlay
 
 LOCAL_PATH := device/Alcatel/idols
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+	LOCAL_KERNEL := device/Alcatel/idols/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/root/fstab.goldfish:root/fstab.goldfish \
+    $(LOCAL_PATH)/root/property_contexts:root/property_contexts \
+    $(LOCAL_PATH)/root/seapp_contexts:root/seap_contexts \
+    $(LOCAL_PATH)/root/sepolicy:root/sepolicy \
+    $(LOCAL_PATH)/root/init:root/init \
+    $(LOCAL_PATH)/root/default.prop:root/default.prop \
+    $(LOCAL_PATH)/root/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/root/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
+    $(LOCAL_PATH)/root/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
+    $(LOCAL_PATH)/root/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
+    $(LOCAL_PATH)/root/init.qcom.ril.sh:root/init.qcom.ril.sh \
+    $(LOCAL_PATH)/root/init.qcom.sh:root/init.qcom.sh \
+    $(LOCAL_PATH)/root/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
+    $(LOCAL_PATH)/root/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    $(LOCAL_PATH)/root/init.goldfish.rc:root/init.goldfish.rc \
+    $(LOCAL_PATH)/root/init.qcom.rc:root/init.qcom.rc \
+    $(LOCAL_PATH)/root/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(LOCAL_PATH)/root/init.target.rc:root/init.target.rc \
+    $(LOCAL_PATH)/root/init.target.usb.rc:root/init.target.usb.rc \
+    $(LOCAL_PATH)/root/init.trace.rc:root/init.trace.rc \
+    $(LOCAL_PATH)/root/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/root/ueventd.qcom.rc:root/ueventd.qcom.rc \
+    $(LOCAL_PATH)/root/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
+    $(LOCAL_PATH)/root/ueventd.rc:root/ueventd.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
